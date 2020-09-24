78b8b550f6a5a54d0b73fb212a88d5787555685d
Saveg <savegdesigner@gmail.com>
2020-09-24 20:51:10 -0300

    redis, session setup :tada:

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,20 @@
 import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
-import mikroOrmConfig from "./mikro-orm.config";
-import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
+import express from "express";
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+
+import mikroOrmConfig from "./mikro-orm.config";
+import { __prod__ } from "./constants";
+import env from "./environment/env";
 
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import { MyContext } from "./types";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
@@ -16,12 +23,34 @@ const main = async () => {
 
   const app = express();
 
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      saveUninitialized: false,
+      secret: env.SESSION_SECRET,
+      resave: false,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
+        sameSite: "lax", //csrf
+        httpOnly: true,
+        secure: __prod__, // cookie only works in https
+      },
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({ em: orm.em }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
   });
 
   apolloServer.applyMiddleware({ app });
